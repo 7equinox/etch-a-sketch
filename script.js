38e17f3901a697db8709a202acf53770e72cd76c
Julius Evan C. Estonilo <116896327+7equinox@users.noreply.github.com>
2025-08-30 22:53:48 +0800

    add commments for js

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const divCntnr = document.querySelector('.container');
 
+// Function to create n^2 grid of square divs
 function createGrid(intGridSize = 16)
 {
     const intTotalSquares = intGridSize ** 2;
 
+    // Loop to create child divs
     for (let i = 0; i < intTotalSquares; i++)
     {
         const divChild = document.createElement('div');
         divChild.classList.toggle('white-box');
 
+        // Access the 4th CSS rule in style.css (currently `.white-box`)
         const styleWhiteBox = document.styleSheets[0].cssRules[3];
+
         const numBoxSize = 704 / intGridSize;
         styleWhiteBox.style.width = `${numBoxSize}px`;
         styleWhiteBox.style.height = `${numBoxSize}px`;
@@ -19,6 +23,7 @@ function createGrid(intGridSize = 16)
 
     const divWhiteBoxes = document.querySelectorAll('.white-box');
 
+    // Set up a 'hover' effect for grid divs
     divWhiteBoxes.forEach((divWhiteBox) => {
         divWhiteBox.addEventListener('mouseover', () => {
             if (!(divWhiteBox.classList.contains('bg-blue')))
@@ -29,8 +34,12 @@ function createGrid(intGridSize = 16)
     });
 }
 
+// Initially call createGrid() with a default grid of square divs
+createGrid();
+
 const btnEditGrid = document.querySelector('button');
 
+// Button to send a popup to customize grid
 btnEditGrid.addEventListener('click', () => {
     const strInput = prompt("[PROMPT] Enter the new number of squares per side (1-100)");
     const intNewSquareCnt = parseInt(strInput);
@@ -45,12 +54,12 @@ btnEditGrid.addEventListener('click', () => {
     }
     else
     {
+        // Remove existing grid
         while (divCntnr.hasChildNodes()) {
             divCntnr.removeChild(divCntnr.firstChild);
         }
 
+        // Generate new grid given number of squares per side
         createGrid(intNewSquareCnt);
     }
 });
-
-createGrid();
