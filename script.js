92532069530769980560274ac86331f9e2d08311
Julius Evan C. Estonilo <116896327+7equinox@users.noreply.github.com>
2025-08-30 19:53:02 +0800

    initialize creating 16x16 grid

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-const INT_SQUARE_COUNT = 16;
+const INT_SQUARE_COUNT = 16 * 16;
 
 const divCntnr = document.querySelector('.container');
 
 for (let i = 0; i < INT_SQUARE_COUNT; i++)
 {
-    const rowDivCntnr = document.createElement('div');
-    divCntnr.appendChild(rowDivCntnr);
-
-    for (let j = 0; j < INT_SQUARE_COUNT; j++)
-    {
-        const colDivCntnr = document.createElement('div');
-        colDivCntnr.setAttribute('class', 'col-container');
-        rowDivCntnr.appendChild(colDivCntnr);
-    }
+    const divChild = document.createElement('div');
+    // divChild.setAttribute('class', `child-${i}`);
+    divChild.classList.toggle('black-box');
+    divCntnr.appendChild(divChild);
 }
