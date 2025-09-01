dd4e9a58c307b2c01bd8437f7e5e017ec682f949
Julius Evan C. Estonilo <116896327+7equinox@users.noreply.github.com>
2025-09-01 11:39:32 +0800

    Refactor the js code

--- script.js
+++ script.js
@@ -1,65 +1,54 @@
-const divCntnr = document.querySelector('.container');
+// Get the external css file (`style.css`)
 const styleSheet = document.styleSheets[0];
 let intCssRuleCount = styleSheet.cssRules.length;
+
+const divCntnr = document.querySelector('.container');
 
 // Function to create n^2 grid of square divs
 function createGrid(intGridSize = 16)
 {
-    const intTotalSquares = intGridSize ** 2;
+    // Customize box size in the same total space (704px wide)
+    const numBoxSize = 704 / intGridSize;
+    styleSheet.insertRule(`
+        .black-box
+        {
+            width: ${numBoxSize}px;
+            height: ${numBoxSize}px;
+            border: 1px solid rgba(0, 0, 0, 0.3);
+            box-sizing: border-box;
+        }
+    `, intCssRuleCount);
 
+    const intTotalSquares = intGridSize ** 2;
     // Loop to create child divs
     for (let i = 0; i < intTotalSquares; i++)
     {
         const divChild = document.createElement('div');
         divChild.classList.add('black-box');
-
-        const numBoxSize = 704 / intGridSize;
-
-        styleSheet.insertRule(`
-            .black-box
-            {
-                width: ${numBoxSize}px;
-                height: ${numBoxSize}px;
-                border: 1px solid rgba(0, 0, 0, 0.3);
-                box-sizing: border-box;
-            }
-        `, intCssRuleCount);
-
         divCntnr.appendChild(divChild);
     }
 
     const divListBlackBox = document.querySelectorAll('.black-box');
-
     // Set up a 'hover' effect for grid divs
     divListBlackBox.forEach((divBlackBox, intIdx) => {
-
+        // Initialize create rule for each 'hover' effect
         styleSheet.insertRule(`
-            .darken-bg-rand-rgb-${intIdx}
-            {
-                background-color: transparent;
-            }
+            .darken-bg-rand-rgb-${intIdx} {  }
         `, intCssRuleCount);
 
         intCssRuleCount++;
         
-        let intOpacityPrcnt = 10;
+        let intOpacityPrcnt = 20;
+        let boolHasBgBlack = false;
 
         divBlackBox.addEventListener('mouseover', () => {
             // Collect current classlist
             const arrCssRule = [...styleSheet.cssRules];
-            // console.log(arrCssRule);
-            // console.log(arrCssRule[3].selectorText === ".darken-bg-rand-rgb-0");
-            // Check if customized bg rule exists
+            // Get the customized bg rule
             const divBlckBxStyle = arrCssRule[intIdx + 2];
 
-            // ???
-            // console.log(divBlckBxStyle.cssText);
-            // divBlckBxStyle.style.setProperty("background-color", "green");
-            // console.log(divBlckBxStyle);
-
-            // divBlckBxStyle.style.setProperty("background-color", "blue");
-            // console.log(divBlckBxStyle.cssText);
-            if (divBlckBxStyle && intOpacityPrcnt <= 100)
+            // If the interactions is less than 10 (opacity % based, x10)
+            if (intOpacityPrcnt <= 100)
             {
                 // Randomize the squares' RGB values with each interaction
                 const intRandRed = Math.ceil(Math.random() * 255);
@@ -74,61 +63,37 @@ function createGrid(intGridSize = 16)
                         ${intRandBlue}
                     )`
                 );
-
                 divBlckBxStyle.style.setProperty(
                     "opacity",
                     `${intOpacityPrcnt}%`
                 );
 
-                console.log(divBlckBxStyle.cssText);
-
                 intOpacityPrcnt += 10;
             }
-            
-            if (intOpacityPrcnt > 100)
+
+            // Achieve a fully black square in only ten interactions.
+            else if (!boolHasBgBlack && intOpacityPrcnt > 100)
             {
                 divBlckBxStyle.style.setProperty(
                     "background-color",
                     "black"
                 );
+                divBlckBxStyle.style.removeProperty(
+                    "opacity"
+                );
 
-                divBlckBxStyle.style.removeProperty("opacity");
+                boolHasBgBlack = true;
             }
 
             divBlackBox.classList.add(`darken-bg-rand-rgb-${intIdx}`);
-            // else
-            // {
-            //     // Randomize the squares' RGB values with each interaction
-            //     const intRandRed = Math.ceil(Math.random() * 255);
-            //     const intRandGreen = Math.ceil(Math.random() * 255);
-            //     const intRandBlue = Math.ceil(Math.random() * 255);
-            //     styleSheet.insertRule(`
-            //         .darken-bg-rand-rgb-${intIdx}
-            //         {
-            //             background-color: rgb(
-            //                 ${intRandRed},
-            //                 ${intRandGreen},
-            //                 ${intRandBlue}
-            //             );
-            //             opacity: ${intOpacityPrcnt}%;
-            //         }
-            //     `, intCssRuleCount);
-
-            //     divBlackBox.classList.add(`darken-bg-rand-rgb-${intIdx}`);
-
-            //     intOpacityPrcnt += 10;
-            // }
         });
     });
-    console.log(intCssRuleCount);
-    console.log(styleSheet.cssRules.length);
 }
 
 // Initially call createGrid() with a default grid of square divs
 createGrid();
 
 const btnEditGrid = document.querySelector('button');
-
 // Button to send a popup to customize grid
 btnEditGrid.addEventListener('click', () => {
     const strInput = prompt("[PROMPT] Enter the new number of squares per side (1-100)");
@@ -154,6 +119,7 @@ btnEditGrid.addEventListener('click', () => {
             divCntnr.removeChild(divCntnr.firstChild);
         }
 
+        // Remove existing custom bg rule
         while(intCssRuleCount > 2)
         {
             styleSheet.deleteRule(2);
@@ -163,5 +129,3 @@ btnEditGrid.addEventListener('click', () => {
         createGrid(intNewSquareCnt);
     }
 });
-
-// TODO: Refactor the code
